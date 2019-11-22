59d5a1ee9c7265b8f82c34981a1d724167edb5aa
bori0409 <44772249+bori0409@users.noreply.github.com>
2019-11-22 12:03:04 +0200

    PlainREST + Model classes
    There is already REST but its empty
    The model classes are matching the DB design

---
+++ MotionREST/Model/MotionModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MotionREST.Motion
+{
+    public class MotionModel
+    {
+        public decimal Pitch
+        {
+            get;set;
+        }
+        public decimal Roll
+        {
+            get;set;
+        }
+        public decimal Yaw { get; set; }
+        public DateTime MyDataTime { get; set; }
+
+        public MotionModel()
+        {
+
+        }
+        public MotionModel(decimal roll, decimal yaw, decimal pitch, DateTime dateTime)
+        {
+            Roll = roll;
+            Pitch = pitch;
+            Yaw = yaw;
+            MyDataTime = dateTime;
+        }
+
+    }
+}
---
+++ MotionREST/Model/PersonModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MotionREST.Model
+{
+    public class PersonModel
+    {
+        public int PersonId;
+        public string PersonName
+        {
+            get;set;
+        }
+        public int MyMotion
+        {
+            get; set;
+
+        }
+        public PersonModel()
+        {
+                
+        }
+        public PersonModel(int id, string name, int motionId)
+        {
+            id = PersonId;
+            name = PersonName;
+            motionId = MyMotion;
+        }
+
+
+    }
+}
